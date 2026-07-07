e4927c6c76f633725197ecb0b6a7836bd1ddd026
1worldonecredit <1creditasia@gmail.com>
2026-07-07 07:58:39 +0700

    edit server.js 03

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const sql = require('mssql');
 const cors = require('cors');
 require('dotenv').config();
 
-const express = require('express');
-const cors = require('cors'); // ตรวจสอบว่ามีบรรทัดนี้
 const app = express();
-
 // --- วาง CORS ตรงนี้เลยครับ ---
 app.use(cors({
     origin: 'https://run9.app', // อนุญาตให้หน้าร้านนี้เข้ามาได้
